fac09bd219ea4692d2be14b3034b720866fc156f
ankush953 <ankushraghuvanshi07@gmail.com>
2020-07-21 09:04:15 +0530

    added pseudo navbar

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./NavigationItem.module.css";
+
+const navigationItem = (props) => (
+  <div className={classes.NavigationItem}>
+    <a href={props.link} className={props.active ? classes.active : null}>
+      {props.children}
+    </a>
+  </div>
+);
+
+export default navigationItem;
