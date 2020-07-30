fa3173eac47b4dda8687c78b89569c0c87bd34f7
ankush953 <ankushraghuvanshi07@gmail.com>
2020-07-30 19:42:23 +0530

    added route to checkout. UI to be done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from "./hoc/Layout/Layout";
 import Checkout from "./containers/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder />
-        <Checkout />
+        <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/" component={BurgerBuilder} />
+        </Switch>
       </Layout>
     </div>
   );
