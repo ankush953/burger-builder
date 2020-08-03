cd1600da1bf2c85e99ccd90d0dbd44bfaf1de8b3
ankush953 <ankushraghuvanshi07@gmail.com>
2020-08-03 09:54:21 +0530

    removed redux-tutorial files

--- src/index.js
+++ src/index.js
@@ -4,25 +4,16 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
-import counterReducer from "./store/reducers/counterReducer";
-import resultReducer from "./store/reducers/resultReducer";
 
-const rootReducer = combineReducers({
-  ctr: counterReducer,
-  res: resultReducer,
-});
-
-const store = createStore(rootReducer);
+const app = (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Provider>
+    <app />
   </BrowserRouter>,
   document.getElementById("root")
 );
--- src/store/actions.js
+++
@@ -1,4 +0,0 @@
-export const INCREMENT = "INCREMENT";
-export const ADD_VALUE = "ADD_VALUE";
-export const STORE_RESULT = "STORE_RESULT";
-export const DELETE_RESULT = "DELETE_RESULT";
