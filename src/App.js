8004230455e660505c23badd344d811289b14c87
ankush953 <ankushraghuvanshi07@gmail.com>
2020-08-03 09:46:05 +0530

    redux tutorial;

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from "./hoc/Layout/Layout";
 import Checkout from "./containers/Checkout/Checkout";
 import { Route, Switch } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
+import Counter from "./containers/Counter/Counter";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
-          <Route path="/" component={BurgerBuilder} />
+          <Route path="/" component={Counter} />
+          {/* <Route path="/" component={BurgerBuilder} /> */}
         </Switch>
       </Layout>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import counterReducer from "./store/reducers/counterReducer";
+import resultReducer from "./store/reducers/resultReducer";
+
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer,
+});
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
