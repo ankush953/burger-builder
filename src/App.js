cd3904e49fc58b5f91376b4a6df8465197bf6bfa
ankush953 <ankushraghuvanshi07@gmail.com>
2020-08-17 16:17:26 +0530

    protected routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from "./hoc/Layout/Layout";
 import Checkout from "./containers/Checkout/Checkout";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
@@ -10,27 +10,45 @@ import { connect } from "react-redux";
 import * as actions from "./store/actions/index";
 
 class App extends Component {
-
-  componentDidMount(){
+  componentDidMount() {
     this.props.onAutoLoginHandler();
   }
 
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" component={BurgerBuilder} />
+      </Switch>
+    );
+
+    if (this.props.isAuth) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" component={BurgerBuilder} />
+        </Switch>
+      );
+    }
+
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" component={BurgerBuilder} />
-          </Switch>
+          {routes}
+          <Redirect to="/" />
         </Layout>
       </div>
     );
   }
 }
+
+const matchStateToProps = (state) => {
+  return {
+    isAuth: state.auth.token !== null,
+  };
+};
 
 const matchDispatchToProps = (dispatch) => {
   return {
@@ -38,4 +56,4 @@ const matchDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(null, matchDispatchToProps)(App);
+export default connect(matchStateToProps, matchDispatchToProps)(App);
